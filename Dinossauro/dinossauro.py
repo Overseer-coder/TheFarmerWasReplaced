f785c5ca18b55ddb25e3b8507fd2f5cb1cc21c67
Pedro Rodrigues <pedrozero20@hotmail.com>
2026-04-01 21:29:49 -0300

    FEAT: mudanca algoritmo do dinossauro para que colha os ossos automaticamente quando preencher toda a fazenda

--- Dinossauro/dinossauro.py
+++ Dinossauro/dinossauro.py
@@ -1,23 +1,37 @@
 horizontal = [West, East]
-fator = [2, 1]
 
-def iniciar():
-	clear()
-	hamiltonian_cycle()
+def iniciar(ossos):
+	while num_items(Items.Bone) < ossos:
+		clear()
+		hamiltonian_cycle()
 
 def hamiltonian_cycle():
 	change_hat(Hats.Dinosaur_Hat)
+	hat = Hats.Dinosaur_Hat
 	dir = North
-	indice_fator = 1
-	while True:
+	while hat == Hats.Dinosaur_Hat:
+		indice_fator = 1
 		while move(North):
 			pass
-		
-		while get_pos_x() != 0 or get_pos_y() != 0:	
-			for _ in range(get_world_size() - fator[indice_fator % 2]):
-				move(horizontal[indice_fator % 2])
-			move(South)
-			indice_fator += 1
-	#change_hat(Hats.Brown_Hat)
+
+		if not move(East) or (get_pos_x() != 0 and get_pos_y() != (get_world_size() - 1)):
+			change_hat(Hats.Brown_Hat)
+			hat = Hats.Brown_Hat
 			
-iniciar()
+		while hat == Hats.Dinosaur_Hat:	
+			for _ in range(get_world_size() - 2):
+				if not move(horizontal[indice_fator % 2]):
+					change_hat(Hats.Brown_Hat)
+					hat = Hats.Brown_Hat
+			if not move(South):
+				if get_pos_x() == 1 and get_pos_y() == 0:
+					break
+				change_hat(Hats.Brown_Hat)
+				hat = Hats.Brown_Hat
+			indice_fator += 1
+			
+		if not move(West):
+			change_hat(Hats.Brown_Hat)
+			hat = Hats.Brown_Hat
+		
+iniciar(10000000)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TheFarmerWasReplaced
-The Farmer Was Replaced é um idle game de programação onde automatizamos drones para gerenciarem uma fazenda, plantando e colhendo. Há desafios extras como a resolução de labirintos e de um desafio como o antigo "Snake"(popular "Jogo da Cobrinha"). Este repositório armazena os scripts utilizados durante o jogo.
+The Farmer Was Replaced é um idle game de programação onde automatizamos drones para gerenciarem uma fazenda.
+
+## Implementações
+**Plantar:** O algoritmo genérico plantar, implementado em **Plantar/plantar.py**, centraliza o plantio de itens baseados em parâmetros recebidos: **"entities"** e **"fator"**. Entities é uma lista de produtos que deverão ser plantados, até no máximo 2. Para que o mesmo produto seja plantado, apenas utilize uma lista com entidade iguais. Fator é uma variável de controle para que sejam plantados produtos de forma alternada, que é um fator determinante para as **"Trees"**.
